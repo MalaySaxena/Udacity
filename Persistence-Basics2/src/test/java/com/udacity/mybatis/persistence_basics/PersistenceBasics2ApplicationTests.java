582b29a29017653acf605de1b96f6abf11b6cedd
Malay <44743128+MalaySaxena@users.noreply.github.com>
2020-08-17 21:49:35 +0530

    Add files via upload

---
+++ Persistence-Basics2/src/test/java/com/udacity/mybatis/persistence_basics/PersistenceBasics2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.mybatis.persistence_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceBasics2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
